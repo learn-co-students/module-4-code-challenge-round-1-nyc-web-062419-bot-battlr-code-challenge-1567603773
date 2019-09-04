c39679e744d841625deb5f9fbb163c72030e72a8
adriennem76 <adrienne.c.miranda@gmail.com>
2019-09-04 11:26:40 -0400

    bonus

--- src/components/FilterBots.js
+++ src/components/FilterBots.js
@@ -3,12 +3,12 @@ import React from "react";
 const FilterBots = props => {
 
     return(
-    <select onChange={props.filterBots}>
-        <option value="">Select Bot Class</option>
-        <option value="Support">Support</option>
-        <option value="Defender">Defender</option>
-        <option value="Assault">Assault</option>
-    </select>
+        <select onChange={props.filterBots}>
+            <option value="">Select Bot Class</option>
+            <option value="Support">Support</option>
+            <option value="Defender">Defender</option>
+            <option value="Assault">Assault</option>
+        </select>
     )
 }
 
